73f4fd8a64ef1ac09d3569f76249129387c83e3e
Javier <javiercastillopena@gmail.com>
2023-07-08 21:21:57 -0500

    feat: Initial project setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# restaurant-review-rest-api
+# Restaurant Review Rest API
+
 REST API built with Node, Express, and MongoDB.
---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import 'dotenv/config';
+
+const server = express();
+const PORT = process.env.PORT;
+
+server.get('/', (req, res) => {
+  res.status(200).json({
+    'message': 'Welcome to the app!'
+  })
+})
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
